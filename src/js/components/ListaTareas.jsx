7a02629d637c02ebfce5a0bb8b0bbf1c9acce3dd
anagit2025 <pintorosbelys90@gmail.com>
2025-06-11 09:45:56 +0000

    proyecto

---
+++ src/js/components/ListaTareas.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import { CrearTarea, GetTareas, EliminarTarea } from "../../services/fetchs";
+
+const ListaTareas = ({ user }) => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const obtenerTareas = async () => {
+    if (user) {
+      const respuesta = await GetTareas(user);
+      if (respuesta?.todos && Array.isArray(respuesta.todos)) {
+        setTareas(respuesta.todos);
+      } else {
+        setTareas([]);
+      }
+    }
+  };
+
+  const agregarTarea = async () => {
+    if (tarea.trim() === "") return;
+
+    await CrearTarea(tarea, user);
+    setTarea("");
+    await obtenerTareas(); 
+  };
+
+  const handleDeleteTarea = async (id) => {
+    await EliminarTarea(id);
+    await obtenerTareas(); 
+  };
+
+  useEffect(() => {
+    obtenerTareas();
+  }, [user]);
+
+  return (
+    <>
+      <div className="crear-tarea">
+        <input
+          type="text"
+          placeholder="Escribe una tarea"
+          value={tarea}
+          onChange={(e) => setTarea(e.target.value)}
+          className="input-usuario"
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && tarea.trim() !== "") {
+              agregarTarea();
+            }
+          }}
+        />
+        <button
+          className="boton-crear mx-2"
+          disabled={tarea.trim() === ""}
+          onClick={agregarTarea}
+        >
+          Agregar
+        </button>
+      </div>
+
+      <h3 className="subtitulo mt-5">Lista de Tareas</h3>
+      <ul className="lista-usuarios">
+        {tareas.map((t, index) => (
+          <li key={t.id ?? index} className="usuario-item">
+            <span>{t.label}</span>
+            <button
+              className="boton-eliminar"
+              onClick={() => handleDeleteTarea(t.id)}
+            >
+              Eliminar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListaTareas;
---
+++ src/services/fetchs.js
@@ -0,0 +1,85 @@
+const apiUrl = "https://playground.4geeks.com/todo";
+
+export const EliminarTarea = async (id) => {
+  try {
+    const response = await fetch(`${apiUrl}/todos/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) throw new Error("Error al eliminar la tarea");
+
+    return alert(`Tarea eliminada exitosamente`);
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Error al eliminar la tarea");
+  }
+};
+
+
+export const CrearTarea = async (tarea, userName) => {
+  try {
+    const response = await fetch(`${apiUrl}/todos/${userName}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ label: tarea, is_done: false }),
+    });
+    if (!response.ok) throw new Error("Error al crear la tarea");
+    else return alert(`Tarea creada exitosamente`);
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Error al crear la tarea");
+  }
+};
+export const EliminarUsuario = async (userName) => {
+  try {
+    const response = await fetch(`${apiUrl}/users/${userName}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) throw new Error("Error al eliminar el usuario");
+
+    return alert(`Usuario "${userName}" eliminado exitosamente`);
+  } catch (error) {
+    console.error("Error:", error);
+    setMensaje("Error al eliminar el usuario");
+  }
+};
+
+export const fetchUsuarios = async () => {
+  try {
+    const response = await fetch(`${apiUrl}/users`);
+    if (!response.ok) throw new Error("Error al obtener la lista de usuarios");
+
+    const data = await response.json();
+    
+
+    return Array.isArray(data.users) ? data.users : [];
+  } catch (error) {
+    console.error("Error en fetchUsuarios:", error);
+    return [];
+  }
+};
+
+export const CrearUsuario = async (userName) => {
+  try {
+    const response = await fetch(`${apiUrl}/users/${userName}`, {method: "POST",headers: { "Content-Type": "application/json" }});
+    if (!response.ok) throw new Error("Error al crear el usuario");
+    usuario = await userName;   // Guardar el usuario creado en la variable global
+    console.log("Usuario creado:", usuario);
+    return alert(`Usuario "${userName}" creado exitosamente`);
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Error al crear el usuario. ¿Ya existe?");
+  }
+};
+
+export const GetTareas = async (userName) => {
+  try {
+    const response = await fetch(`${apiUrl}/users/${userName}`);
+    if (!response.ok) throw new Error("Error al obtener las tareas");
+    const data = await response.json();
+    
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
